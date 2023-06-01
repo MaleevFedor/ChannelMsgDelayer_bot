5aafe605f2756e8cca92b15e04833e09a09f880e
Administrator <keysec@keemail.me>
2023-06-01 21:34:00 +0300

    FSM in process

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from data import db_session
 from data.user_class import User
-
 import config
-
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,6 +13,15 @@ dp = Dispatcher(bot)
 
 db_session.global_init("data.db3")
 
+class ForwardingMessages(StatesGroup):
+    WaitingForMessage = State()
+
+
+
+def register_handlers_forwarding(dp: Dispatcher):
+    dp.register_message_handler('', commands="food", state="*")
+    dp.register_message_handler('', state=''.waiting_for_food_name)
+    dp.register_message_handler('', state=''.waiting_for_food_size)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
@@ -28,14 +37,25 @@ async def start(message: types.Message):
 
 
 @dp.message_handler(content_types=[types.ContentType.ANY])
-async def echo(message: types.Message):
+#async def echo(message: types.Message):
+#
+#    await bot.send_photo(chat_id=-1001945938118, photo=message.photo[-1].file_id,
+#                             caption=message.caption)
+#    for i in message:
+#        await message.answer(i)
 
+async def start_forwarding(message: types.Message, state: FSMContext):
+    await message.answer('Скиньте сообщение для пересылки')
+    await state.set_state(ForwardingMessages.WaitingForMessage.state)
+
+
+
+async def forward(message: types.Message):
     if message.text is not None:
         await bot.send_message(-1001945938118, message.text)
     elif message.photo is not None:
         await bot.send_photo(chat_id=-1001945938118, photo=message.photo[-1].file_id,
                              caption=message.caption)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False)

a4dbc1608d666a24b07ab1fb4d7bb26d04c5eef6
Administrator <keysec@keemail.me>
2023-06-01 19:33:21 +0300

    channel get id

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ dp = Dispatcher(bot)
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
-
+    #await bot.send_message()
+    await message.answer(message['forward_from_chat']['id'])
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False)

6f37cece8fd6c33fe09f013d50f286fe12f85285
Administrator <keysec@keemail.me>
2023-06-06 01:10:28 +0300

    unfinished get_message()

--- fsm.py
+++ fsm.py
@@ -3,6 +3,9 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class ForwardingMessages(StatesGroup):
     WaitingForMessage = State()
+    WaitingForTimeToSchedule = State()
+    WaitingForChannelsToBeChosen = State()
+
 
 
 class AddChannels(StatesGroup):
